418754a6e0b69779073718541d907494b3d9b432
mjdude <mo@syncspot.net>
2016-04-10 23:20:11 +0100

    added validation

--- server.js
+++ server.js
@@ -32,17 +32,18 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
-    console.log('description: ' + body.description);
+    var body = _.pick(req.body, 'description' , 'completed');
 
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+      return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId;
     todos.push(body);
     todoNextId++;
 
     res.json(body);
-
-
-
 
 });
 

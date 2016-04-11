64498815ce31407e50fc9a5a444db40f511fc161
mjdude <mo@syncspot.net>
2016-04-11 15:21:30 +0100

    Added PUT alternative

--- server.js
+++ server.js
@@ -67,24 +67,30 @@ app.delete('/todos/:id',function(req, res){
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res){
-  var body = _.pick( req.body, "description","completed", "id");
+  var todoId = parseInt(req.params.id);
+  var matchedToDo = _.findWhere(todos, {id:todoId});
+  var body = _.pick( req.body, "description","completed");
+  var validAttributes = {};
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 || !_.isNumber(body.id) ){
+  if (!matchedToDo) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
     return res.status(400).send();
   }
 
-  var todoId = parseInt(req.params.id);
-  var matchedToDo = _.findWhere(todos, {id:todoId});
-
-  if (matchedToDo) {
-    matchedToDo.id = body.id;
-    matchedToDo.completed = body.completed;
-    matchedToDo.description = body.description.trim();
-    res.json(matchedToDo);
-
-  }else {
-    res.status(404).json({"error" : "no todo found with that id"});
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0 ) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
   }
+
+  _.extend(matchedToDo , validAttributes);
+
+  res.json(matchedToDo);
 
 });
 

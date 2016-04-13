c9b9000fc21f874035474c4ef903c5dbfcdc1d20
mjdude <mo@syncspot.net>
2016-04-13 11:10:16 +0100

    add postgres for production

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined , undefined, {
-  'dialect' : 'sqlite',
-  'storage' : __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production'){
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    'dialect' : 'postgres'
+  });
+} else {
+  sequelize = new Sequelize(undefined, undefined , undefined, {
+    'dialect' : 'sqlite',
+    'storage' : __dirname + '/data/dev-todo-api.sqlite'
+  });
+}
 
 var db = {};
 
@@ -11,7 +20,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
-// var sequelize = New Sequelize( undefined, undefined,undefined, {
-//
-// });

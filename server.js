99ce9bb8ce9ee72eb04218c551119b04292a767c
mjdude <mo@syncspot.net>
2016-04-19 21:40:23 +0100

    added login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -144,6 +145,30 @@ app.post('/users' , function(req, res){
     var body = _.pick(req.body, 'email', 'password');
     db.user.create(body).then(function(user){
       res.json(user.toPublicJSON());
+    }, function(e){
+      res.status(400).json(e);
+    });
+});
+
+// POST /users/login
+
+app.post('/users/login' , function(req, res){
+    var body = _.pick(req.body, 'email', 'password');
+    var where = {};
+
+    if (body.hasOwnProperty('email') && body.email.length > 0) {
+      where.email = body.email;
+    }
+
+    db.users.findAll({
+      where : where
+    }).then(function(user){
+      var hashedPassword = bcrypt.hashSync(body.password , user.salt);
+      if (user.hashedPassword = hashedPassword) {
+          res.json(user);
+      } else {
+        res.status(404).send();
+      }
     }, function(e){
       res.status(400).json(e);
     });

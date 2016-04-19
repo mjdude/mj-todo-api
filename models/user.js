37c26e543296352049c451f2e50ac2b6462e2a7a
mjdude <mo@syncspot.net>
2016-04-19 21:24:26 +0100

    hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,33 +1,50 @@
-module.exports = function(sequelize, Datatypes){
-  return sequelize.define('user', {
-    email: {
-      type: Datatypes.STRING,
-      allowNull : false,
-      unique: true,
-      validate : {
-        isEmail: true
-      }
-    },
-    password: {
-      type : Datatypes.STRING,
-      allowNull: false,
-      validate : {
-        len : [7,100],
-      }
-    },
-  },{
-      hooks : {
-        beforeValidate : function(user, options){
-          if(typeof user.email === 'string'){
-              user.email = user.email.toLowerCase();
-          }
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, Datatypes) {
+    return sequelize.define('user', {
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        salt: {
+            type: Datatypes.STRING
+        },
+        password_hash: {
+            type: Datatypes.STRING
+        },
+        password: {
+            type: Datatypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                len: [7, 100],
+            },
+            set: function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+            }
+        },
+    }, {
+        hooks: {
+            beforeValidate: function(user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
+            }
+        },
+
+        instanceMethods: {
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, "id", "email", "updatedAt", "createdAt");
+            }
         }
-    }
-
-
-  }
-
-
-
-);
+    });
 };

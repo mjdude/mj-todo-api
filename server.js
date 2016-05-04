40622fecbff7e7d342eb1047934b3b83d1458778
mjdude <mo@syncspot.net>
2016-05-04 16:36:56 +0100

    added DELETE /users/login

--- server.js
+++ server.js
@@ -195,6 +195,16 @@ app.post('/users/login', function(req, res) {
 
 });
 
+// DELETE /users/login
+
+app.delete('/users/login', middleware.requireAuthentication ,function(req, res){
+  req.token.destroy().then(function(){
+    res.status(204).send();
+  }).catch(function(){
+    res.status(500).send();
+  });
+});
+
 db.sequelize.sync({
      force: true
 }).then(function() {
